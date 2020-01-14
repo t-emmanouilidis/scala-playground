03acf692b6160818cbe48878b226e636440174aa
Tryfon Emmanouilidis <tremmanouilidis@gmail.com>
2020-01-15 01:45:00 +0200

    Added additional test prop code

--- src/main/scala/net/tremman/scala/sample/test/property/Prop.scala
+++ src/main/scala/net/tremman/scala/sample/test/property/Prop.scala
@@ -1,12 +1,26 @@
 package net.tremman.scala.sample.test.property
 
-import net.tremman.scala.sample.state.{RNG, State}
-import net.tremman.scala.sample.test.property.Prop.{FailedCase, SuccessCount, TestCases}
+import java.util.concurrent.Executors
 
-case class Gen[A](sample: State[RNG, A]) {
+import net.tremman.scala.sample.parallel.Par.Par
+import net.tremman.scala.sample.state.{RNG, State}
+import net.tremman.scala.sample.test.property.Prop.{FailedCase, MaxSize, SuccessCount, TestCases}
+
+case class Gen[+A](sample: State[RNG, A]) {
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap((a: A) => f(a).sample))
 
   def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(n => Gen.listOfN(n, this))
+
+  def listOfN(size: Int): Gen[List[A]] = Gen.listOfN(size, this)
+
+  def unsized: SGen[A] = SGen(_ => this)
+
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+
+  def map2[B, C](anotherGen: Gen[B])(f: (A, B) => C) =
+    Gen(sample.map2(anotherGen.sample)(f))
+
+  def **[B](anotherGen: Gen[B]): Gen[(A, B)] = (this map2 anotherGen) ((_, _))
 }
 
 object Gen {
@@ -26,8 +40,21 @@ object Gen {
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
 
-  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] =
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
 
+    Gen(State(RNG.double).flatMap(d => if (d < threshold) g1._1.sample else g2._1.sample))
+  }
+
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n))
+
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n.max(1)))
+}
+
+case class SGen[+A](forSize: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = forSize(n)
+
+  def map[B](f: A => B): SGen[B] = SGen(forSize(_).map(f))
 }
 
 sealed trait Result {
@@ -42,25 +69,45 @@ case class Failed(failure: FailedCase, successes: SuccessCount) extends Result {
   override def isFailure: Boolean = true
 }
 
-case class Prop(run: (TestCases, RNG) => Result) {
-  def check: Result = ???
+case object Proved extends Result {
+  override def isFailure: Boolean = true
+}
 
-  def &&(p: Prop): Prop = {
-    new Prop() {
-      override def check: Result = None
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+
+  def &&(p: Prop): Prop = Prop(
+    (max: MaxSize, n: TestCases, rng: RNG) => run(max, n, rng) match {
+      case Passed | Proved => p.run(max, n, rng)
+      case failed => failed
     }
-  }
+  )
+
+  def ||(p: Prop): Prop = Prop(
+    (max: MaxSize, n: TestCases, rng: RNG) => run(max, n, rng) match {
+      case Failed(_, _) => p.run(max, n, rng)
+      case x => x
+    }
+  )
 
 }
 
 object Prop {
+  type MaxSize = Int
   type FailedCase = String
   type SuccessCount = Int
   type TestCases = Int
-  type Result = Option[(FailedCase, SuccessCount)]
 
-  def forAll[A](theGenerator: Gen[A])(f: A => Boolean): Prop = Prop((n, rng) =>
-    randomStream(theGenerator)(rng).zip(Stream.from(0)).take(n).map {
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop = forAll((n: Int) => g.forSize(n))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop((max, n, rng) => {
+    val casesPerSize = (n + (max - 1)) / max
+    val props: Stream[Prop] = Stream.from(0).take((n.min(max)) + 1).map(i => forAll(g(i))(f))
+    val prop: Prop = props.map(p => Prop((max, _, rng) => p.run(max, casesPerSize, rng))).toList.reduce(_ && _)
+    prop.run(max, n, rng)
+  })
+
+  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = Prop((max, n, rng) =>
+    randomStream(gen)(rng).zip(Stream.from(0)).take(n).map {
       case (a, i) => try {
         if (f(a)) Passed else Failed(a.toString, i)
       } catch {
@@ -75,5 +122,31 @@ object Prop {
     s"test case: $a\n" +
       s"generated an exception: ${ex.getMessage}\n" +
       s"stack trace:\n ${ex.getStackTrace.mkString("\n")}"
+
+  def run(p: Prop,
+          maxSize: MaxSize = 100,
+          testCases: TestCases = 100,
+          rng: RNG = RNG.SimpleRNG(System.currentTimeMillis())): Unit = {
+    p.run(maxSize, testCases, rng) match {
+      case Failed(msg, successes) =>
+        println(s"! Failed after $successes passed tests:${System.lineSeparator()}$msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
+  }
+
+  def check(p: => Boolean): Prop = Prop((_, _, _) => {
+    if (p) Passed else Failed("()", 0)
+  })
+
+  val S = Gen.weighted(Gen.choose(1, 4).map(Executors.newFixedThreadPool) -> 0.75,
+    Gen.unit(Executors.newCachedThreadPool) -> 0.25)
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) {
+      case (es, a) => f(a)(es).get()
+    }
 
 }
--- src/test/scala/net/tremman/scala/sample/test/property/PropTest.scala
+++ src/test/scala/net/tremman/scala/sample/test/property/PropTest.scala
@@ -1,6 +1,12 @@
 package net.tremman.scala.sample.test.property
 
+import java.util.concurrent.Executors
+
+import net.tremman.scala.sample.parallel.Par
+import net.tremman.scala.sample.parallel.Par.Par
 import org.scalatest.FunSuite
+
+import scala.annotation.tailrec
 
 class PropTest extends FunSuite {
 
@@ -14,5 +20,45 @@ class PropTest extends FunSuite {
     // Assert
     assert(listGen.isInstanceOf[Gen[Int]])
   }
+
+  test("listOf.first") {
+    val smallInt = Gen.choose(-10, 10)
+    val maxProp = Prop.forAll(Gen.listOf1(smallInt))(ls => {
+      val max = ls.max
+      !ls.exists(_ > max)
+    })
+    Prop.run(maxProp)
+  }
+
+  test("listOf.sorted") {
+    val smallInt = Gen.choose(-10, 10)
+    val sortedProp = Prop.forAll(Gen.listOf1(smallInt))(ls => {
+      @tailrec
+      def isSorted(ls: List[Int]): Boolean = ls match {
+        case _ :: Nil => true
+        case x :: xs => x < xs.head && isSorted(xs)
+        case _ => true
+      }
+
+      isSorted(ls)
+    })
+    Prop.run(sortedProp)
+  }
+
+  test("par.map.unit") {
+    val es = Executors.newCachedThreadPool
+
+    def equal[A](one: Par[A], other: Par[A]): Par[Boolean] = Par.map2(one, other)(_ == _)
+
+    val mapProp = Prop.check {
+      equal(
+        Par.map(Par.unit(1))(_ + 1),
+        Par.unit(2)
+      )(es).get()
+    }
+    Prop.run(mapProp)
+  }
+
+
 
 }
