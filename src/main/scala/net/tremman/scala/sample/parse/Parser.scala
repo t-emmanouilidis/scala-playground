ccffe45d81a4bb33a9cfdef7fd051af7192ddd0a
Tryfon Emmanouilidis <tremmanouilidis@gmail.com>
2020-01-20 07:55:52 +0200

    more parsing stuff

--- src/main/scala/net/tremman/scala/sample/parse/Parser.scala
+++ src/main/scala/net/tremman/scala/sample/parse/Parser.scala
@@ -1,7 +1,10 @@
 package net.tremman.scala.sample.parse
 
 trait ParseError {
+  def msg: String
+}
 
+case class BasicParseError(msg: String) extends ParseError {
 }
 
 class Parser[A] {
@@ -23,14 +26,28 @@ trait Parsers[ParseError, Parser[+_]] {
 
   implicit def string(str: String): Parser[String]
 
+  // this is the implicit function used to convert a Parser instance to a ParserOps instance
   implicit def operators[A](theParser: Parser[A]): ParserOps[A] = ParserOps(theParser)
 
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def zeroOrMore(str: String): Parser[Int] = map(many(str))(_.size)
+
+  def oneOrMore(str: String): Parser[Int]
+
+  def followedBy[A](one: Parser[A], another: Parser[A]): Parser[A]
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
 
   case class ParserOps[A](one: Parser[A]) {
     def |[B >: A](another: Parser[B]): Parser[B] = self.or(one, another)
 
     def or[B >: A](another: => Parser[B]): Parser[B] = self.or(one, another)
+
+    def followedBy[B >: A](another: Parser[B]): Parser[B] =
+      self.followedBy(one, another)
   }
 
 }
--- src/test/scala/net/tremman/scala/sample/parse/ParsersTest.scala
+++ src/test/scala/net/tremman/scala/sample/parse/ParsersTest.scala
@@ -5,15 +5,72 @@ import org.scalatest.FunSuite
 class ParsersTest extends FunSuite {
 
   test("char.basic") {
-    val parsers: Parsers[ParseError, Parser[Char]] = null
+    val parsers: Parsers[ParseError, Parser[Char]] = ???
     val c: Char = 'c'
     assert(parsers.run(parsers.char(c))(c.toString) === Right(c))
   }
 
   test("string.basic") {
-    val parsers: Parsers[ParseError, Parser[String]] = null
+    val parsers: Parsers[ParseError, Parser[String]] = ???
     val str: String = "aString"
     assert(parsers.run(parsers.string(str))(str) === Right(str))
+  }
+
+  test("or.basic") {
+    val parsers: Parsers[ParseError, Parser[String]] = ???
+    import parsers._
+    val theParser: Parser[String] = "abra" | "cadabra"
+    assert(parsers.run(theParser)("abra") === Right("abra"))
+    assert(parsers.run(theParser)("cadabra") === Right("cadabra"))
+  }
+
+  test("or parser should be commutative") {
+    val parsers: Parsers[ParseError, Parser[String]] = ???
+    import parsers._
+    val oneParser: Parser[String] = "abra" | "cadabra"
+    val otherParser: Parser[String] = "cadabra" | "abra"
+    assert(parsers.run(oneParser)("abra") === parsers.run(otherParser)("abra"))
+  }
+
+  test("or parser should be associative") {
+    val parsers: Parsers[ParseError, Parser[String]] = ???
+    import parsers._
+    val oneParser = "a".or("b").or("c")
+    val otherParser = "b".or("c").or("a")
+    assert(parsers.run(oneParser)("abc") === parsers.run(otherParser)("abc"))
+  }
+
+  test("list.basic") {
+    val parsers: Parsers[ParseError, Parser[String]] = ???
+    import parsers._
+    assert(run(listOfN(3, "ab" | "cad"))("ababcad") === Right("ababcad"))
+    assert(run(listOfN(3, "ab" | "cad"))("cadabab") === Right("cadabab"))
+    assert(run(listOfN(3, "ab" | "cad"))("cadabab") === Right("ababab"))
+  }
+
+  test("basic int zero or more") {
+    val parsers: Parsers[ParseError, Parser[Int]] = ???
+    import parsers._
+    assert(run(zeroOrMore("a"))("aaa") === Right(3))
+    assert(run(zeroOrMore("aaa"))("aaa") === Right(1))
+    assert(run(zeroOrMore("a"))("b123") === Right(0))
+  }
+
+  test("basic int one or more") {
+    val parsers: Parsers[ParseError, Parser[Int]] = ???
+    import parsers._
+    assert(run(oneOrMore("a"))("aaa") === Right(3))
+    assert(run(oneOrMore("aaa"))("aaa") === Right(1))
+    assert(run(oneOrMore("a"))("b123") === Left(BasicParseError("Expected one or more 'a'")))
+  }
+
+  test("basic followed by") {
+    val parsers: Parsers[ParseError, Parser[Int]] = ???
+    import parsers._
+    // the first parser is converted implicitly to a ParserOps instance which has the method followedBy
+    assert(run(zeroOrMore("a").followedBy(oneOrMore("b")))("aaab") === Right((3, 1)))
+    assert(run(zeroOrMore("a").followedBy(oneOrMore("b")))("bbb") === Right((0, 3)))
+    assert(run(zeroOrMore("a").followedBy(oneOrMore("b")))("aaaab") === Right((4, 1)))
   }
 
 }
